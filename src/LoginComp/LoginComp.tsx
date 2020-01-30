9e6792ae57feabcebebc5195eace5d28935de902
Ahder <benzinered@yahoo.fr>
2020-01-30 01:12:59 +0100

    Login feature

--- src/LoginComp/LoginComp.tsx
+++ src/LoginComp/LoginComp.tsx
@@ -1,22 +1,42 @@
 import React, {Component} from "react";
+import {LoginCall} from "./LoginCall";
 
-export default class LoginComp extends Component {
+export default class LoginComp extends Component<any, any> {
+
+    constructor(props: any ) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            status: ""
+        };
+        this.lunchLogin = this.lunchLogin.bind(this);
+    }
+
+    async lunchLogin() {
+        let isConnected = await LoginCall(this.state.email, this.state.password) || false;
+        return isConnected ? this.setState({status: 'Connected !'}) : this.setState({status: 'Error in credentials'});
+        //TODO: remonter l'info aau parent
+    }
+
     render() {
         return (
+            <div>
             <form>
                 <h3>Login</h3>
-
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email"/>
+                    <input type="email" className="form-control" placeholder="Enter email" onChange={e => this.setState({email: e.target.value})}/>
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password"/>
+                    <input type="password" className="form-control" placeholder="Enter password" onChange={e => this.setState({password: e.target.value})}/>
                 </div>
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <button type="button" className="btn btn-primary btn-block" onClick={this.lunchLogin}>Submit</button>
             </form>
+            <p>{this.state.status}</p>
+            </div>
         );
     }
 }
