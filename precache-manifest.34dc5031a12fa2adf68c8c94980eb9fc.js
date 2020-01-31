76dc6d3c541f3e34932f1b012fa9d6d37684d52f
Ahder <benzinered@yahoo.fr>
2020-01-31 23:11:14 +0100

    Updates

---
+++ precache-manifest.34dc5031a12fa2adf68c8c94980eb9fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6cf72d8ace62de49debea3ed429aa35",
+    "url": "/BookingApp/index.html"
+  },
+  {
+    "revision": "f6cc405258a9c517f451",
+    "url": "/BookingApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "89af7559ccf7aad9419d",
+    "url": "/BookingApp/static/js/2.0c66f8eb.chunk.js"
+  },
+  {
+    "revision": "3d416322b0ec4a5a91d5be4afe7c848c",
+    "url": "/BookingApp/static/js/2.0c66f8eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6cc405258a9c517f451",
+    "url": "/BookingApp/static/js/main.fb7a8a01.chunk.js"
+  },
+  {
+    "revision": "2be4b167cb65d8f150fb",
+    "url": "/BookingApp/static/js/runtime-main.9634bad7.js"
+  }
+]);
